4d91de8675ec513bbb29ed8abd3d47c81443a81a
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-06 17:00:20 +0800

    Add item processing

--- pchardwarescraper/pipelines.py
+++ pchardwarescraper/pipelines.py
@@ -5,9 +5,28 @@
 
 
 # useful for handling different item types with a single interface
+import re
 from itemadapter import ItemAdapter
-
+from pchardwarescraper.utils.categorymap import categories
 
 class PchardwarescraperPipeline:
     def process_item(self, item, spider):
+        if spider.name != 'mobo':
+            adapter = ItemAdapter(item)
+            field_names = adapter.field_names()
+            for field_name in field_names:
+                adapter[field_name] = adapter.get(field_name).strip()
+            
+            adapter['price'] = self.clean_and_convert_value(adapter.get('price'))
+            adapter['category'] = categories.get(adapter.get('category'))
+
+        
         return item
+    
+    def clean_and_convert_value(self, value):
+        # Remove non-numeric characters
+        cleaned_value = re.sub(r'[^0-9.]', '', value)
+        float_value = float(cleaned_value)
+        decimal_value = round(float_value, 2)
+
+        return decimal_value
---
+++ pchardwarescraper/utils/categorymap.py
@@ -0,0 +1,13 @@
+categories = {
+    "processor": "Processor",
+    "Processors": "Processor",
+    "Graphics": "Video card",
+    "video-cards-64": "Video card",
+    "Motherboards": "Motherboard",
+    "motherboards-61": "Motherboard",
+    "processors-62": "Processor",
+    "processor": "Processor",
+    "graphics-card": "Video card",
+    "motherboard": "Motherboard",
+    "Graphics": "Video card",
+}
