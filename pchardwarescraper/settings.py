7e29de6f38fe4d82867bdec7c4105efd25250fa4
Eoan <gabriel.naoe@adamson.edu.ph>
2023-06-12 20:41:29 +0800

    Revert settings py

--- pchardwarescraper/settings.py
+++ pchardwarescraper/settings.py
@@ -12,6 +12,9 @@ BOT_NAME = "pchardwarescraper"
 SPIDER_MODULES = ["pchardwarescraper.spiders"]
 NEWSPIDER_MODULE = "pchardwarescraper.spiders"
 
+# Splash config
+SPLASH_URL = 'http://192.168.59.103:8050'
+
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "pchardwarescraper (+http://www.yourdomain.com)"
@@ -44,15 +47,24 @@ DOWNLOAD_DELAY = 3
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "pchardwarescraper.middlewares.PchardwarescraperSpiderMiddleware": 543,
-#}
 
+SPIDER_MIDDLEWARES = {
+   #  "pchardwarescraper.middlewares.PchardwarescraperSpiderMiddleware": 543,
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "pchardwarescraper.middlewares.PchardwarescraperDownloaderMiddleware": 543,
-#}
+
+DOWNLOADER_MIDDLEWARES = {
+   #  "pchardwarescraper.middlewares.PchardwarescraperDownloaderMiddleware": 543,
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
+
+HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
